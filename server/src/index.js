89c600889fbf6a6c575e89847bec6f9f6efb9216
Sava Stanchev <s.stanchev94@gmail.com>
2021-04-02 12:48:47 +0300

    basic src/index.js and npm run start cmd

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+const PORT = 5555;
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
