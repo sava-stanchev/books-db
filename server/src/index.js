77fb5ef7bdaa1542d7e31edf51585c9275023074
Sava Stanchev <s.stanchev94@gmail.com>
2021-04-02 14:26:56 +0300

    post login JWT token authentication check

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
+import '../load-env.js';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import jwt from 'jsonwebtoken';
+import authenticateToken from './middlewares/authenticate-token.js';
 
 const PORT = 5555;
 
@@ -9,5 +12,12 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const user = { name: username };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken: accessToken });
+});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
