94045fdecc9f76be9e74afd2eea5fe540e2e095a
Svetoslav Hrankov <7938581-Hrankov@users.noreply.gitlab.com>
2021-04-04 17:56:11 +0300

    book validators

--- server/src/middlewares/validate-body.js
+++ server/src/middlewares/validate-body.js
@@ -1,14 +1,16 @@
 import errorStrings from '../common/error-strings.js';
 
 export default (resource, validator) => (req, res, next) => {
-  const errors = {};
+  const errors = {}; //???
     Object.keys(validator).forEach(key => {
       if (!validator[key](req.body[key])) {
         errors[key] = errorStrings[resource][key];
       }
     });
+
     if (Object.keys(errors).length > 0) {
         return res.status(400).json({ errors });
     }
+    
     next();
 };
