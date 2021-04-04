b01ab6895ac41d18e1513007d65140a1bc53fa76
Svetoslav Hrankov <7938581-Hrankov@users.noreply.gitlab.com>
2021-04-04 18:11:48 +0300

    validators

--- server/src/common/error-strings.js
+++ server/src/common/error-strings.js
@@ -1,5 +1,14 @@
 export default {
-  books:{
+  users: {
+    userName: `Expected string with length [1 - 30]`,
+    password: `Expected string with length [1 - 30]`,
+    firstName: `Expected string with length [1 - 20]`,
+    lastName: `Expected string with length [1 - 20]`,
+    age: `Expected number bigger than 1`,
+    gender: `Expected string with length [1 - 30]`,
+    e_mail: `Expected valid e-mail`,
+  },
+  books: {
     title: 'Expected string with length [1 - 256]',
     author:'Expected string with length [1 - 256]',
     genre: 'Expected string with length [1 - 40]',
@@ -10,7 +19,12 @@ export default {
     printLength: 'Expected number bigger than 1',
     isDeleted: 'Expected boolean',
     isBorrowed: 'Expected boolean',
-  }
+  },
+  reviews: {
 
+  },
+  reviews: {
+
+  },
 
 };
---
+++ server/src/validators/user-create-validator.js
@@ -0,0 +1,20 @@
+export default {
+//   -	number – auto, unique 
+// -	username – string - [3 - 30], no special characters, unique
+// -	password – string – [7 - 20], one uppercase, one number
+// -	firstName – string – [1 - 20]
+// -	lastName – string – [1 - 20]
+// -	age – number > 0 || null
+// -	gender – string (list)
+// -	e-mail – string [6 - 40], include ‘@’, no unique
+// -	isAdmin – Boolean
+// -	isDeleted – Boolean
+// -	isBanned – Boolean
+userName: (value) => typeof value === 'string' && value.length > 3 && value.length < 30,
+password: (value) => typeof value === 'string' && value.length > 7 && value.length < 20,
+firstName: (value) => typeof value === 'string' && value.length > 1 && value.length < 20,
+lastName: (value) => typeof value === 'string' && value.length > 1 && value.length < 20,
+age: (value) => typeof value === 'number' && value > 0,
+gender: (value) => typeof value === 'string' && value.length > 1 && value.length < 20,
+e_mail: (value) => typeof value === 'string' && value.length > 6 && value.includes('@'),
+}
