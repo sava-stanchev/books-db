b117409eec191890d33fd135623c9d526766d935
Sava Stanchev <s.stanchev94@gmail.com>
2021-04-11 13:07:45 +0300

    new auth-middleware added

---
+++ server/src/auth/auth-middleware.js
@@ -0,0 +1,20 @@
+import passport from 'passport';
+
+const authMiddleware = passport.authenticate('jwt', { session: false });
+
+const roleMiddleware = roleName => {
+    return (req, res, next) => {
+        if (req.user && req.user.role === roleName) {
+            next();
+        } else {
+            res.status(403).send({
+                message: 'Resource is forbidden.'
+            })
+        }
+    };
+};
+
+export {
+    authMiddleware,
+    roleMiddleware
+};
