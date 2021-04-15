25795c5d229b2eb43a86702d872371b55316f039
Sava Stanchev <s.stanchev94@gmail.com>
2021-04-15 10:45:34 +0300

    try-catch added to index

--- server/src/index.js
+++ server/src/index.js
@@ -45,15 +45,20 @@ app.use(passport.initialize());
 // register user - work
 app.post('/users', loggedUserGuard, async (req, res) => {
     const user = req.body;
-    user.password = await bcrypt.hash(user.password, 10);
+    try {
+        user.password = await bcrypt.hash(user.password, 10);
 
-    const newUser = await usersData.createUser(user);
-    if (newUser.error) {
-        return res.status(400).json(newUser.response);
+        const newUser = await usersData.createUser(user);
+        if (newUser.error) {
+            return res.status(400).json(newUser.response);
+        }
+
+        res.json(newUser.response);
+    } catch (error) {
+        res.status(400).json({
+            error: error.message
+        });
     }
-
-    res.json(newUser.response);
-
 });
 
 // login  - work
@@ -83,125 +88,173 @@ app.post('/login', async (req, res) => {
 
 // logout - work
 app.delete('/logout', authMiddleware, async (req, res) => {
-    await logoutUser(req.headers.authorization.replace('Bearer ', ''));
+    try {
+        await logoutUser(req.headers.authorization.replace('Bearer ', ''));
 
-    res.json({
-        message: 'Successfully logged out!'
-    });
+        res.json({
+            message: 'Successfully logged out!'
+        });
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
+        });
+    }
 });
 
 // retrieve all books - work
 app.get('/books', authMiddleware, loggedUserGuard, async (req, res) => {
-    const {
-        title,
-        sort
-    } = req.query;
-    if (sort) {
-        const theBooksSortedByYear = await booksData.sortBooksByYear(sort);
-        return res.json(theBooksSortedByYear);
+    const { title, sort } = req.query;
+    try {
+        if (sort) {
+            const theBooksSortedByYear = await booksData.sortBooksByYear(sort);
+            return res.json(theBooksSortedByYear);
+        }
+        if (title) {
+            const theBooksFoundByTitle = await booksData.searchBooksByTitle(title);
+            return res.json(theBooksFoundByTitle);
+        }
+
+        const theBooks = await booksData.getAllBooks();
+        res.json(theBooks);
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
+        });
     }
-    if (title) {
-        const theBooksFoundByTitle = await booksData.searchBooksByTitle(title);
-        return res.json(theBooksFoundByTitle);
-    }
-    const theBooks = await booksData.getAllBooks();
-    res.json(theBooks);
 });
 
 // create new book - in admin  - work to check validator
 app.post('/admin/books', authMiddleware, loggedUserGuard, roleAuth(userRole.Admin), validateBody('book', bookCreateValidator), async (req, res) => {
-    console.log(req.user);
-    const book = await booksData.createBook(req.body, req.user);
-
-    res.json(book);
+    try {
+        const book = await booksData.createBook(req.body, req.user);
+        res.json(book);
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
+        });
+    }
 });
 
 // view individual book by id - work
 app.get('/books/:id', authMiddleware, loggedUserGuard, async (req, res) => {
-    res.json(await booksData.getBookById(+req.params.id))
+    try {
+        res.json(await booksData.getBookById(+req.params.id));
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
+        });
+    }   
 });
 
 // update any book as admin
 app.put('/admin/books/:id', authMiddleware, loggedUserGuard, roleAuth(userRole.Admin), validateBody('book', bookUpdateValidator), async (req, res) => {
-    const {
-        id
-    } = req.params;
+    const { id } = req.params;
     const updateData = req.body;
-    const updatedBook = await booksService.updateBook(+id, updateData);
+    
+    try {
+        const updatedBook = await booksService.updateBook(+id, updateData);
 
-    if (!updatedBook) {
-        res.status(404).send({
-            message: 'Book not found!'
-        });
-    } else {
-        res.send({
-            message: 'Book updated!'
+        if (!updatedBook) {
+            res.status(404).send({
+                message: 'Book not found!'
+            });
+        } else {
+            res.send({
+                message: 'Book updated!'
+            });
+        }
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
         });
     }
 });
 
 // delete any book as admin
 app.delete('/admin/books/:id', authMiddleware, loggedUserGuard, roleAuth(userRole.Admin), async (req, res) => {
-    await booksData.deleteBook(+req.params.id);
-    res.json({
-        message: `Book deleted`,
-    });
+    try {
+        await booksData.deleteBook(+req.params.id);
+        res.json({
+            message: `Book deleted`,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
+        });
+    }
 });
 
 // borrow a book by id - working
 app.post('/books/:id', authMiddleware, loggedUserGuard, async (req, res) => {
-    const {
-        id
-    } = req.params;
-    const theBook = await booksData.getBookById(+id);
-    if (!theBook) {
-        return res.status(404).json({
-            msg: `Book with id ${id} was not found!`
-        });
-    }
-    const bookBorrowed = await booksData.borrowBook(+id);
-    if (!bookBorrowed) {
-        res.json({
-            msg: `Book has already been borrowed!`
-        });
-    } else {
-        res.json({
-            msg: 'Book successfully borrowed!'
+    const { id } = req.params;
+    try {
+        const theBook = await booksData.getBookById(+id);
+        if (!theBook) {
+            return res.status(404).json({
+                msg: `Book with id ${id} was not found!`
+            });
+        }
+
+        const bookBorrowed = await booksData.borrowBook(+id);
+        if (!bookBorrowed) {
+            res.json({
+                msg: `Book has already been borrowed!`
+            });
+        } else {
+            res.json({
+                msg: 'Book successfully borrowed!'
+            });
+        }
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
         });
     }
 });
 
 // return a book by id - work
 app.patch('/books/:id', authMiddleware, loggedUserGuard, async (req, res) => {
-    const book = await booksData.returnBook(+req.params.id);
-    if (!book) {
-        res.json({
-            msg: `Book has already been returned!`
-        });
-    } else {
-        res.json({
-            msg: 'Book successfully returned!'
+    try {
+        const book = await booksData.returnBook(+req.params.id);
+        if (!book) {
+            res.json({
+                msg: `Book has already been returned!`
+            });
+        } else {
+            res.json({
+                msg: 'Book successfully returned!'
+            });
+        }
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
         });
     }
 });
 
 // read all reviews for a book - working
 app.get('/books/:id/reviews', authMiddleware, loggedUserGuard, async (req, res) => {
-    const {
-        id
-    } = req.params;
-    const theBook = await booksData.getBookById(+id);
-    if (!theBook) {
-        return res.status(404).json({
-            msg: `Book with id ${id} was not found!`
-        });
-    }
-    const theReviews = await reviewsData.getReviewsForBook(+id);
-    if (theReviews.length > 0) {
-        res.send(theReviews);
-    } else {
-        return res.json({
-            msg: 'Book has no reviews yet!'
+    const { id } = req.params;
+
+    try {
+        const theBook = await booksData.getBookById(+id);
+        if (!theBook) {
+            return res.status(404).json({
+                msg: `Book with id ${id} was not found!`
+            });
+        }
+
+        const theReviews = await reviewsData.getReviewsForBook(+id);
+        if (theReviews.length > 0) {
+            res.send(theReviews);
+        } else {
+            return res.json({
+                msg: 'Book has no reviews yet!'
+            });
+        }
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
         });
     }
 });
@@ -217,6 +270,7 @@ app.post('/books/:books_id/reviews', authMiddleware, loggedUserGuard, async (req
                 msg: `Book was not found!`
             });
         }
+        
         const check = (await reviewsData.userReviewByBookId(userId, bookId))[0];
         if (check) {
             return res.status(200).json({
@@ -238,63 +292,80 @@ app.patch('/reviews/:reviewId', authMiddleware, loggedUserGuard, async (req, res
     const reviewId = req.params.reviewId;
     const updateData = req.body;
 
-    const review = await reviewsData.getReviewById(+reviewId);
+    try {
+        const review = await reviewsData.getReviewById(+reviewId);
 
-    if (!review) {
-        res.status(404).send({
-            message: 'Review not found!'
-        });
-    }
+        if (!review) {
+            res.status(404).send({
+                message: 'Review not found!'
+            });
+        }
 
-    if (review.users_id !== req.user.user_id) {
-        return res.status(403).json({
-            message: 'You are not authorized to update this review!'
-        })
-    }
+        if (review.users_id !== req.user.user_id) {
+            return res.status(403).json({
+                message: 'You are not authorized to update this review!'
+            })
+        }
 
-    const reviewUpdated = await reviewService.updateReview(+reviewId, updateData);
+        const reviewUpdated = await reviewService.updateReview(+reviewId, updateData);
 
-    if (reviewUpdated) {
-        res.send({
-            message: 'Review updated!'
+        if (reviewUpdated) {
+            res.send({
+                message: 'Review updated!'
+            });
+        }
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
         });
     }
 });
 
 // delete book review - work
 app.delete('/reviews/:reviews_id', authMiddleware, loggedUserGuard, async (req, res) => {
+    try {
+        const review = await reviewsData.getReviewById(req.params.reviews_id);
+        if (!review || review.is_deleted === 1) {
+            res.status(400).json({
+                message: 'Review not found!'
+            });
+        }
+        if (review.users_id !== req.user.user_id) {
+            return res.status(403).json({
+                message: 'You are not authorized to delete this review!'
+            })
+        }
+        await reviewsData.deleteReview(req.params.reviews_id);
 
-    const review = await reviewsData.getReviewById(req.params.reviews_id);
-    if (!review || review.is_deleted === 1) {
-        res.status(400).json({
-            message: 'Review not found!'
+        res.status(200).json({
+            message: `Review deleted`,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
         });
     }
-    if (review.users_id !== req.user.user_id) {
-        return res.status(403).json({
-            message: 'You are not authorized to delete this review!'
-        })
-    }
-    await reviewsData.deleteReview(req.params.reviews_id);
-
-    res.status(200).json({
-        message: `Review deleted`,
-    });
 });
 
 // delete any review from admin - work
 app.delete('/admin/reviews/:reviews_id', authMiddleware, loggedUserGuard, roleAuth(userRole.Admin), async (req, res) => {
-    const review = await reviewsData.getReviewById(req.params.reviews_id);
-    if (!review || review.is_deleted === 1) {
-        res.status(400).json({
-            message: 'Review not found!'
+    try {
+        const review = await reviewsData.getReviewById(req.params.reviews_id);
+        if (!review || review.is_deleted === 1) {
+            res.status(400).json({
+                message: 'Review not found!'
+            });
+        }
+        await reviewsData.deleteReview(req.params.reviews_id);
+
+        res.status(200).json({
+            message: `Review deleted`,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
         });
     }
-    await reviewsData.deleteReview(req.params.reviews_id);
-
-    res.status(200).json({
-        message: `Review deleted`,
-    });
 });
 
 // update any review from admin
@@ -322,10 +393,16 @@ app.put('/admin/reviews/:reviews_id', authMiddleware, loggedUserGuard, roleAuth(
 
 // ban user 
 app.post('/admin/users/:id/ban', authMiddleware, loggedUserGuard, roleAuth(userRole.Admin), async (req, res) => {
-    await usersData.banUser(+req.params.id);
-    res.json({
-        message: `User (${req.params.id}) banned!`
-    });
+    try {
+        await usersData.banUser(+req.params.id);
+        res.json({
+            message: `User (${req.params.id}) banned!`
+        });
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
+        });
+    }
 });
 
 // delete user - work
@@ -369,21 +446,30 @@ app.put('/admin/users/:id', authMiddleware, loggedUserGuard, roleAuth(userRole.A
             error: error.message
         });
     }
-
-
 })
 
 // get all users - work
 app.get('/admin/users', authMiddleware, loggedUserGuard, async (req, res) => {
-    const users = await usersData.getAllUsers();
-    res.json(users);
+    try {
+        const users = await usersData.getAllUsers();
+        res.json(users);
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
+        });
+    }
 })
 
 // read reviews as admin
 app.get('/admin/reviews', authMiddleware, loggedUserGuard, async (req, res) => {
-    const reviews = await reviewsData.getAllReviews();
-
-    res.send(reviews)
+    try {
+        const reviews = await reviewsData.getAllReviews();
+        res.send(reviews)
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
+        });
+    }
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
