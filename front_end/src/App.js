b704d093e0c2beb33fc68a8afe867aad62e8a392
Sava Stanchev <s.stanchev94@gmail.com>
2021-04-21 15:22:03 +0300

    very basic navbar

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import {Navbar, Nav} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <h1>HELLO!!!</h1>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Library</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#books">Books</Nav.Link>
+            <Nav.Link href="#reviews">Reviews</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#login">Login</Nav.Link>
+            <Nav.Link eventKey={2} href="#register">
+              Register
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
