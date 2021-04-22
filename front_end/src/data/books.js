04807da0b80e24fe4a6cae53632317d19f7e555a
Sava Stanchev <s.stanchev94@gmail.com>
2021-04-22 17:42:15 +0300

    add data for books to test with later

---
+++ front_end/src/data/books.js
@@ -0,0 +1,25 @@
+const books = [
+    {
+      title: 'Interesting times',
+      author: 'Terry Pratchett',
+      genre: 2,
+      age_recommendation: 12,
+      isbn: '0575058005',
+      publishing_year: 1995,
+      language: 3,
+      print_length: 236,
+    },
+    {
+        title: 'Mort',
+        author: 'Terry Pratchett',
+        genre: 2,
+        age_recommendation: 12,
+        isbn: '0575000000',
+        publishing_year: 1955,
+        language: 2,
+        print_length: 136,
+    },
+];
+
+export default books;
+  
