9ded35dcde50c4d63308d9f4e3944a45b74b82af
Sava Stanchev <s.stanchev94@gmail.com>
2021-04-22 08:03:27 +0300

    login view with bootstrap

--- front_end/src/components/Login.js
+++ front_end/src/components/Login.js
@@ -1,9 +1,32 @@
+import {Form, Button, Col, Row, Image} from 'react-bootstrap';
+
 const Login = () => {
-    return(
-        <div className="App">
-            <h1>Login</h1>
-        </div>
-    )
+  return(
+    <div className="App">
+      <Row className="landing">
+        <Col>
+          <Form style={{width:"70%", marginLeft:"10%", marginTop:"30%"}}>
+            <Form.Group controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control placeholder="Enter username" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </Form>
+        </Col>
+        <Col>
+          <div>
+            <Image src="./img/login-img.jpg" thumbnail style={{border:"none"}} />
+          </div>
+        </Col>
+      </Row>
+    </div>
+  )
 };
 
 export default Login;
