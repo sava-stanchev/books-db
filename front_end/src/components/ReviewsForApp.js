7e7bab6013db055696a8176cbc8058bf3f92fb07
Sava Stanchev <s.stanchev94@gmail.com>
2021-04-25 21:35:09 +0300

    reviews edit and save

--- front_end/src/components/ReviewsForApp.js
+++ front_end/src/components/ReviewsForApp.js
@@ -3,21 +3,34 @@ import reviewsData from '../data/reviews.js';
 import Reviews from './Reviews.js';
 
 const ReviewsForApp = () => {
-    const [reviews, setReviews] = useState(reviewsData);
+  const [reviews, setReviews] = useState(reviewsData);
 
-    const removeReview = (reviews_id) => {
-        setReviews(prevReviews => prevReviews.filter(r => r.reviews_id !== reviews_id));
-    }
+  const removeReview = (reviews_id) => {
+    setReviews(prevReviews => prevReviews.filter(r => r.reviews_id !== reviews_id));
+  }
 
-    return(
-        <div className="App">
-            {reviews && reviews.map(r => (
-                <div>
-                    <Reviews {...r} removeReview={removeReview} key={r.reviews_id} />
-                </div>
-            ))}
-        </div>
-    )
+  const updateReview = (reviews_id, reviewData) => {
+    const index = reviews.findIndex(r => r.reviews_id === reviews_id);
+    const copy = [...reviews];
+
+    copy[index] = {...copy[index], ...reviewData};
+    setReviews(copy);
+  }
+
+  return(
+    <div className="App">
+      {reviews.map((r) => {
+        return (
+          <Reviews
+            {...r}
+            update={(updateData) => updateReview(r.reviews_id, updateData)} 
+            remove={() => removeReview(r.reviews_id)}
+            key={r.reviews_id}
+          />
+        );
+      })}
+    </div>
+  )
 }
 
 export default ReviewsForApp;
