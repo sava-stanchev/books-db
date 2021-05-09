56969efa724fcee1d7f851b54448bc6cf7796d88
Sava Stanchev <s.stanchev94@gmail.com>
2021-05-09 12:58:28 +0300

    like/dislike button

--- front_end/src/components/SingleBookReviews.js
+++ front_end/src/components/SingleBookReviews.js
@@ -4,6 +4,7 @@ import {Button} from "react-bootstrap";
 import {FaTrashAlt} from "react-icons/fa";
 import {FaEdit} from "react-icons/fa";
 import {FaThumbsUp} from "react-icons/fa";
+import {FaThumbsDown} from "react-icons/fa";
 import { HOST } from '../common/constants.js';
 
 const SingleBookReviews = ({id}) => {
@@ -63,6 +64,12 @@ const SingleBookReviews = ({id}) => {
                   <h6>{review.content}</h6>
                   <p>by <i>{review.user_name}</i> on {new Date(review.date_created).toLocaleDateString("en-US")}</p>
                   <div>
+                    <Button variant="warning" className="reviewBtns">
+                      <FaThumbsUp/>
+                    </Button>
+                    <Button variant="warning" className="reviewBtns">
+                      <FaThumbsDown/>
+                    </Button>
                     <Button variant="primary" className="reviewBtns" onClick={() => history.push(`/reviews/${review.reviews_id}/update`)}>
                       <FaEdit/>
                     </Button>
