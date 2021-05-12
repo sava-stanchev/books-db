b01b6700f1c7b7c7dfe634afada69a11c20838c0
Sava Stanchev <s.stanchev94@gmail.com>
2021-05-12 20:01:26 +0300

    upload poster

--- front_end/src/components/UploadFile.js
+++ front_end/src/components/UploadFile.js
@@ -1,32 +1,25 @@
 import {useState} from 'react';
-import {Form} from "react-bootstrap";
-import {HOST} from '../common/constants.js';
 
 const UploadPoster = () => {
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState();
 
-  const uploadFile = async (ev) => {
-    ev.preventDefault();
-    const fileData = new FormData();
-    fileData.set('posters', file);
+  const send = event => {
+    const data = new FormData();
+    data.append('file', file);
 
-    const response = await fetch(`${HOST}/books/cover`, {
-      method: 'PUT',
-      headers: {
-        'authorization': `bearer ${localStorage.getItem('token')}`
-      },
-      body: fileData
-    });
-
-    const data = await response.json();
-  }
+    console.log(data);
+  };
   
   return (
-    <Form>
-      <Form.Group>
-        <Form.File id="the-file" label="Upload book cover" onChange={(e) => setFile(e.target.files[0])}/>
-      </Form.Group>
-    </Form>
+    <form action="#">
+      <div className="flex">
+        <label htmlFor="file">Book cover: </label>
+        <input type="file" id="file" accept=".jpg" onChange={event => {
+          const file = event.target.files[0];
+          setFile(file);
+        }} />
+      </div>
+    </form>
   )
 }
 
