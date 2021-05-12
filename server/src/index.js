1c88a2bfdf40ca6aea9e1b591abdaadbfbe25e2e
Sava Stanchev <s.stanchev94@gmail.com>
2021-05-12 12:33:02 +0300

    import

--- server/src/index.js
+++ server/src/index.js
@@ -19,8 +19,6 @@ import reviewService from './services/review-service.js';
 import reviewsLikeData from './data/reviewsLike.js';
 import userService from './services/user-service.js';
 import booksController from './controllers/booksController.js';
-// import booksData from './data/books.js/';
-
 
 const config = dotenv.config().parsed;
 
