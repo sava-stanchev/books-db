828c9037258e4adaac8002c51e3392c5f32fddc0
Sava Stanchev <s.stanchev94@gmail.com>
2021-05-12 04:44:03 +0300

    clean up code in register

--- front_end/src/components/Register.js
+++ front_end/src/components/Register.js
@@ -1,24 +1,22 @@
 import {Jumbotron} from "react-bootstrap";
 import RegistrationForm from './RegistrationForm';
 import {useEffect, useState} from 'react';
-import { HOST } from '../common/constants.js';
+import {HOST} from '../common/constants.js';
 
 const Register = () => {
   const [genders, setGenders] = useState(null);
-  // const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState(null);
-  
+
   useEffect(() => {
     fetch(`${HOST}/genders`, {
       method: 'GET',
     })
     .then((response) => response.json())
     .then((data) => setGenders(data));
-   // .catch((error) => setError(error.message))
-   // .finally(() => setLoading(false));
   }, []);
 
-  if(genders === null) return <div>Loading...</div>
+  if (genders === null) {
+    return <div>Loading...</div>;
+  }
 
   return(
     <div className="registration-page-bg-info">
