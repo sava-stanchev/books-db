827a42dee5fc8bf433db4092d064f300b3872b58
Sava Stanchev <s.stanchev94@gmail.com>
2024-03-17 12:51:17 +0200

    new bootstrap login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,58 @@
-import './App.css';
+import "./App.css";
 import NavBar from "./components/Navbar";
 import Books from "./components/Books";
-import Login from './components/Login';
-import Register from './components/Register';
-import SingleBook from './components/SingleBook';
-import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
-import {useState} from 'react';
-import AuthContext, {getUser} from './providers/auth-context';
-import AddBook from './components/AddBook';
-import AddReview from './components/AddReview';
-import UpdateBook from './components/UpdateBook';
-import Users from './components/Users';
-import SingleUser from './components/SingleUser'
-import UpdateReview from './components/UpdateReview';
-import HomePage from './components/HomePage';
-import GuardedRoute from './providers/GuardedRoute';
+import Login from "./components/Login";
+import Register from "./components/Register";
+import SingleBook from "./components/SingleBook";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { useState } from "react";
+import AuthContext, { getUser } from "./providers/auth-context";
+import AddBook from "./components/AddBook";
+import AddReview from "./components/AddReview";
+import UpdateBook from "./components/UpdateBook";
+import Users from "./components/Users";
+import SingleUser from "./components/SingleUser";
+import UpdateReview from "./components/UpdateReview";
+import HomePage from "./components/HomePage";
+import GuardedRoute from "./providers/GuardedRoute";
 
 const App = () => {
   const [authValue, setAuthValue] = useState({
     user: getUser(),
     isLoggedIn: Boolean(getUser()),
-  })
+  });
 
   return (
     <BrowserRouter>
-      <AuthContext.Provider value = {{...authValue, setAuthState: setAuthValue}}>
+      <AuthContext.Provider
+        value={{ ...authValue, setAuthState: setAuthValue }}
+      >
         <NavBar />
         <Switch>
           <Redirect path="/" exact to="/home" />
-          <Route exact path="/home" component={HomePage}/>
+          <Route exact path="/home" component={HomePage} />
           <GuardedRoute exact path="/books" component={Books} />
           <GuardedRoute exact admin path="/books/create" component={AddBook} />
           <GuardedRoute exact path="/books/:id" component={SingleBook} />
           <GuardedRoute exact path="/books/:id/update" component={UpdateBook} />
-          <GuardedRoute exact path="/books/:id/create-review" component={AddReview} />
-          <GuardedRoute exact path="/reviews/:reviewId/update" component={UpdateReview} />
+          <GuardedRoute
+            exact
+            path="/books/:id/create-review"
+            component={AddReview}
+          />
+          <GuardedRoute
+            exact
+            path="/reviews/:reviewId/update"
+            component={UpdateReview}
+          />
           <GuardedRoute exact admin path="/users" component={Users} />
           <GuardedRoute exact path="/profile" component={SingleUser} />
-          <Route path="/login" component={Login}/>       
-          <Route path="/register" component={Register}/>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
         </Switch>
-      </AuthContext.Provider>    
+      </AuthContext.Provider>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,74 +1,83 @@
-import {useContext, useState} from 'react';
-import {useHistory} from 'react-router-dom';
-import {HOST} from '../common/constants';
-import decode from 'jwt-decode';
-import AuthContext from '../providers/auth-context';
-import AlertModal from './AlertModal';
+import { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { HOST } from "../common/constants";
+import decode from "jwt-decode";
+import AuthContext from "../providers/auth-context";
 
 const Login = () => {
   const history = useHistory();
   const auth = useContext(AuthContext);
-  const [isOpen, setIsOpen] = useState(false);
-  const [alertMsg, setAlertMsg] = useState(null);
   const [user, setUser] = useState({
-    username: '',
-    password: '',
+    email: "",
+    password: "",
   });
 
   const updateUser = (prop, value) => {
     setUser({
       ...user,
       [prop]: value,
-    })
+    });
   };
-  
+
   const login = (e) => {
     e.preventDefault();
     fetch(`${HOST}/login`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'content-type': 'application/json',
+        "content-type": "application/json",
       },
       body: JSON.stringify(user),
     })
-    .then(res => res.json())
-    .then((res) => {
-      if (res.message) {
-        setAlertMsg(res.message);
-        setIsOpen(true);
-      } else {
-        localStorage.clear();
-        localStorage.setItem('token', res.token);
-        const user = decode(res.token);
-        auth.setAuthState({user, isLoggedIn: true});
-        history.push('/home');
-      }
-    })
-    .catch(() => history.push('/500'));
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.message) {
+          setAlertMsg(res.message);
+          setIsOpen(true);
+        } else {
+          localStorage.clear();
+          localStorage.setItem("token", res.token);
+          const user = decode(res.token);
+          auth.setAuthState({ user, isLoggedIn: true });
+          history.push("/home");
+        }
+      })
+      .catch(() => history.push("/500"));
   };
 
-  return(
-    <section className="join-login-main-section">
-      <AlertModal open={isOpen} onClose={() => setIsOpen(false)} alertMsg={alertMsg} />
-      <h1 className="join-login-text">
-        Welcome
-        <span className="accent-text">!</span>
-      </h1>
-      <form className="join-login-form">
-        <div className="input-group">
-          <label>Username:</label>
-          <input type="text" onChange={e => updateUser('username', e.target.value)}/>
-        </div>
-        <div className="input-group">
-          <label>Password:</label>
-          <input type="password" onChange={e => updateUser('password', e.target.value)}/>
-        </div>
-        <div className="input-group">
-          <button className="btn" onClick={(e) => login(e)}>Sign in</button>
-        </div>
-      </form>
-    </section>
-  )    
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 bg-dark">
+      <div className="form-container p-5 rounded bg-light">
+        <form>
+          <h3 className="text-center">Sign In</h3>
+          <div className="mb-4">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter Email"
+              className="form-control"
+              onChange={(e) => updateUser("email", e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter Password"
+              className="form-control"
+              onChange={(e) => updateUser("password", e.target.value)}
+            />
+          </div>
+          <div className="d-grid">
+            <button className="btn btn-primary" onClick={(e) => login(e)}>
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
-  
+
 export default Login;
