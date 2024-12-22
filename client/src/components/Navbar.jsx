489779891b4ee3ec87230335a0e3c879d4633531
Sava Stanchev <s.stanchev94@gmail.com>
2024-12-22 20:11:21 +0200

    sign out logic custom hook + simplify nav conditional render

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,35 +6,39 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 
-const NavbarComponent = () => {
+const useSignOut = () => {
   const auth = useContext(AuthContext);
   const navigate = useNavigate();
 
-  async function signOut(request) {
+  const signOut = async () => {
     try {
-      const response = await fetch(request);
+      const logoutRequest = new Request(`${HOST}/logout`, {
+        method: "DELETE",
+        headers: {
+          authorization: `bearer ${localStorage.getItem("token")}`,
+        },
+      });
+
+      const response = await fetch(logoutRequest);
 
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
-      } else {
-        auth.setAuthState({
-          user: null,
-          isLoggedIn: false,
-        });
-        localStorage.removeItem("token");
-        navigate("/");
       }
+
+      auth.setAuthState({ user: null, isLoggedIn: false });
+      localStorage.removeItem("token");
+      navigate("/");
     } catch (error) {
       console.error(error.message);
     }
-  }
+  };
 
-  const logoutRequest = new Request(`${HOST}/logout`, {
-    method: "DELETE",
-    headers: {
-      authorization: `bearer ${localStorage.getItem("token")}`,
-    },
-  });
+  return signOut;
+};
+
+const NavbarComponent = () => {
+  const { isLoggedIn, user } = useContext(AuthContext);
+  const signOut = useSignOut();
 
   return (
     <>
@@ -51,9 +55,9 @@ const NavbarComponent = () => {
               </Nav.Link>
             </Nav>
             <Nav>
-              {auth.isLoggedIn ? (
+              {isLoggedIn ? (
                 <>
-                  {auth.user.is_admin === 1 && (
+                  {user?.is_admin === 1 && (
                     <Nav.Link as={Link} to="/users">
                       Users
                     </Nav.Link>
@@ -61,9 +65,7 @@ const NavbarComponent = () => {
                   <Nav.Link as={Link} to="/books">
                     Books
                   </Nav.Link>
-                  <Nav.Link onClick={() => signOut(logoutRequest)}>
-                    Log Out
-                  </Nav.Link>
+                  <Nav.Link onClick={signOut}>Log Out</Nav.Link>
                 </>
               ) : (
                 <>
