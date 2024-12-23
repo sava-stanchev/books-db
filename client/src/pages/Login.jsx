9e207cd47c38a7d8d220dcf71292b4803b89c86c
Sava Stanchev <s.stanchev94@gmail.com>
2024-12-23 20:00:06 +0200

    login simplify code, improve input handling, better structure

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,122 +1,108 @@
 import { useContext, useState } from "react";
-import { HOST } from "src/common/constants";
-import decode from "jwt-decode";
-import AuthContext from "src/utils/auth-context";
 import { useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import decode from "jwt-decode";
+
+import { HOST } from "src/common/constants";
+import AuthContext from "src/utils/auth-context";
 import AlertDismissible from "src/components/Alert";
 
 const Login = () => {
   const navigate = useNavigate();
   const auth = useContext(AuthContext);
-  const [user, setUser] = useState({
-    username: "",
-    password: "",
-  });
+
+  const [user, setUser] = useState({ username: "", password: "" });
   const [showPassword, setShowPassword] = useState(false);
-  const [activeAlert, setActiveAlert] = useState(false);
-  const [alertMessage, setAlertMessage] = useState(null);
+  const [alert, setAlert] = useState({ active: false, message: "" });
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
+  const togglePasswordVisibility = () => setShowPassword((prev) => !prev);
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setUser((prev) => ({ ...prev, [id]: value }));
   };
 
-  const updateUser = (prop, value) => {
-    setUser({
-      ...user,
-      [prop]: value,
-    });
-  };
-
-  async function signIn(request) {
-    setActiveAlert(false);
-
+  const signIn = async () => {
+    setAlert({ active: false, message: "" });
     try {
-      const response = await fetch(request);
+      const response = await fetch(`${HOST}/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user),
+      });
+
       const result = await response.json();
-
       if (!response.ok) {
-        setAlertMessage(result.message);
-        throw new Error(`Response status: ${response.status}`);
-      } else {
-        localStorage.clear();
-        localStorage.setItem("token", result.token);
-        const user = decode(result.token);
-        auth.setAuthState({ user, isLoggedIn: true });
-        navigate("/");
+        throw new Error(result.message || "Failed to authenticate");
       }
-    } catch (error) {
-      setActiveAlert(true);
-      console.error(error.message);
-    }
-  }
 
-  const loginRequest = new Request(`${HOST}/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  });
+      localStorage.clear();
+      localStorage.setItem("token", result.token);
+      const decodedUser = decode(result.token);
+      auth.setAuthState({ user: decodedUser, isLoggedIn: true });
+      navigate("/");
+    } catch (error) {
+      console.error(error.message);
+      setAlert({ active: true, message: error.message });
+    }
+  };
 
   return (
-    <>
+    <div className="d-flex justify-content-center align-items-center flex-fill bg-dark">
       <div className="position-absolute start-0 end-0">
-        <div className="d-flex justify-content-center col-md-12">
-          <AlertDismissible
-            activeAlert={activeAlert}
-            alertMessage={alertMessage}
-          />
-        </div>
+        <AlertDismissible
+          activeAlert={alert.active}
+          alertMessage={alert.message}
+        />
       </div>
-      <div className="d-flex justify-content-center align-items-center vh-100 bg-dark">
-        <div className="form-container p-5 rounded bg-light">
-          <form>
-            <h3 className="text-center">Sign In</h3>
-            <div className="mb-4">
-              <label htmlFor="username">Username</label>
+      <div className="form-container p-5 rounded bg-light">
+        <form>
+          <h3 className="text-center">Sign In</h3>
+          <div className="mb-4">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              placeholder="Enter Username"
+              className="form-control"
+              value={user.username}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password">Password</label>
+            <div className="position-relative">
               <input
-                type="text"
-                id="username"
-                placeholder="Enter Username"
-                className="form-control"
-                onChange={(e) => updateUser("username", e.target.value)}
+                type={showPassword ? "text" : "password"}
+                id="password"
+                placeholder="Enter Password"
+                className="form-control pe-5"
+                value={user.password}
+                onChange={handleChange}
               />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="password">Password</label>
-              <div className="position-relative">
-                <input
-                  type={showPassword ? "text" : "password"}
-                  id="password"
-                  placeholder="Enter Password"
-                  className="form-control pe-5"
-                  onChange={(e) => updateUser("password", e.target.value)}
-                />
-                <button
-                  className="password-eye-icon"
-                  type="button"
-                  onClick={togglePasswordVisibility}
-                  aria-label="Toggle password visibility"
-                >
-                  {showPassword ? <FaEyeSlash /> : <FaEye />}
-                </button>
-              </div>
-            </div>
-            <div className="d-grid">
               <button
-                className="btn btn-primary"
                 type="button"
-                onClick={() => signIn(loginRequest)}
-                disabled={!user.username || !user.password}
+                className="password-eye-icon"
+                onClick={togglePasswordVisibility}
+                aria-label="Toggle password visibility"
               >
-                Sign in
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
               </button>
             </div>
-          </form>
-        </div>
+          </div>
+          <div className="d-grid">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={signIn}
+              disabled={!user.username || !user.password}
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
