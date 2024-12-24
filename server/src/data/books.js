1eb6fa7e8797c3bb75819b8338dbd90bf8a6e312
Sava Stanchev <s.stanchev94@gmail.com>
2024-12-24 18:52:24 +0200

    books sql - implicit table context, destructure, order by clause

--- server/src/data/books.js
+++ server/src/data/books.js
@@ -1,62 +1,70 @@
 import pool from "./pool.js";
 
 const getAllBooks = async () => {
-  const result = await pool.query(`
-    SELECT * FROM books b
-    WHERE b.is_deleted != 1
-  `);
-  return result[0];
-};
-
-const sortBooksByYear = async (sort) => {
-  if (sort === "year_asc") {
-    return await pool.query(`
-      SELECT * FROM books b
-      WHERE b.is_deleted != 1
-      ORDER BY b.year
-    `);
-  }
-
-  if (sort === "year_desc") {
-    return await pool.query(`
-      SELECT * FROM books b
-      WHERE b.is_deleted != 1
-      ORDER BY b.year DESC
-    `);
-  }
-};
-
-const getBookById = async (bookId) => {
   const sql = `
-    SELECT b.id, b.title, b.author, b.year, b.cover, b.description, l.language, g.genre, b.avg_rating, b.num_ratings, b.is_deleted
+    SELECT * FROM books
+    WHERE is_deleted != 1
+  `;
+
+  const [result] = await pool.query(sql);
+  return result;
+};
+
+const sortBooksByYear = async (sortOrder) => {
+  let orderClause;
+  if (sortOrder === "year_asc") {
+    orderClause = "ORDER BY year ASC";
+  } else if (sortOrder === "year_desc") {
+    orderClause = "ORDER BY year DESC";
+  } else {
+    throw new Error("Invalid sort order");
+  }
+
+  const sql = `
+    SELECT * FROM books
+    WHERE is_deleted != 1
+    ${orderClause}
+  `;
+
+  const [result] = await pool.query(sql);
+  return result;
+};
+
+const getBookById = async (id) => {
+  const sql = `
+    SELECT 
+      b.id, b.title, b.author, b.year, b.cover, b.description, 
+      l.language, g.genre, b.avg_rating, b.num_ratings, b.is_deleted
     FROM books AS b
-    JOIN languages AS l
-    ON b.language = l.id
-    JOIN genres AS g
-    ON b.genre = g.id
-    WHERE b.is_deleted != 1 and b.id = ?
+    JOIN languages AS l ON b.language = l.id
+    JOIN genres AS g ON b.genre = g.id
+    WHERE b.is_deleted != 1 AND b.id = ?
   `;
 
-  const result = await pool.query(sql, [bookId, bookId]);
-  return result[0];
+  const [result] = await pool.query(sql, [id]);
+  return result;
 };
 
-const deleteBook = async (bookId) => {
+const deleteBook = async (id) => {
   const sql = `
-    UPDATE books SET books.is_deleted = 1
-    WHERE books.id = ?
+    UPDATE books
+    SET is_deleted = 1
+    WHERE id = ?
   `;
-  await pool.query(sql, [bookId]);
+
+  const [result] = await pool.query(sql, [id]);
+  return result.affectedRows > 0;
 };
 
-const updateBookRating = async (bookId, rating) => {
+const updateBookRating = async (id, rating) => {
   const sql = `
-    UPDATE books AS b
-    SET b.avg_rating = (b.num_ratings / (b.num_ratings + 1)) * b.avg_rating + (1 / (b.num_ratings + 1)) * ${rating},
-    b.num_ratings = b.num_ratings + 1
-    WHERE b.id = ?
+    UPDATE books
+    SET 
+      avg_rating = (num_ratings / (num_ratings + 1)) * avg_rating + (1 / (num_ratings + 1)) * ?,
+      num_ratings = num_ratings + 1
+    WHERE id = ?
   `;
-  await pool.query(sql, [bookId]);
+  await pool.query(sql, [rating, id]);
 };
 
 export default {
