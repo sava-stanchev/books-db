7e31cac3913c269a833a9369119222b2770f5212
Sava Stanchev <s.stanchev94@gmail.com>
2024-12-26 18:17:29 +0200

    books controller - add asyncHandler instead of try-catch

--- server/src/controllers/books-controller.js
+++ server/src/controllers/books-controller.js
@@ -1,4 +1,5 @@
 import express from "express";
+import asyncHandler from "express-async-handler";
 import booksData from "../data/books.js";
 import bookRatingsData from "../data/book-ratings.js";
 import reviewsData from "../data/reviews.js";
@@ -10,21 +11,25 @@ const booksController = express.Router();
 booksController
 
   // Get all books
-  .get("/", authMiddleware, loggedUserGuard, async (req, res) => {
-    try {
+  .get(
+    "/",
+    authMiddleware,
+    loggedUserGuard,
+    asyncHandler(async (req, res) => {
       const { sort } = req.query;
       const books = sort
         ? await booksData.sortBooksByYear(sort)
         : await booksData.getAllBooks();
       res.json(books);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  })
+    })
+  )
 
   // Get a book
-  .get("/:id", authMiddleware, loggedUserGuard, async (req, res) => {
-    try {
+  .get(
+    "/:id",
+    authMiddleware,
+    loggedUserGuard,
+    asyncHandler(async (req, res) => {
       const bookId = +req.params.id;
       const book = await booksData.getBookById(bookId);
       if (!book) {
@@ -32,34 +37,28 @@ booksController
       } else {
         res.json(book);
       }
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  })
+    })
+  )
 
   // Get rating for book by user
   .post(
     "/:id/user-rating",
     authMiddleware,
     loggedUserGuard,
-    async (req, res) => {
-      try {
-        const bookId = req.params.id;
-        const userId = req.body.id;
-        const userRating = await bookRatingsData.hasUserRatedBook(
-          userId,
-          bookId
-        );
-        res.json({ rating: userRating?.rating || 0 });
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
-    }
+    asyncHandler(async (req, res) => {
+      const bookId = req.params.id;
+      const userId = req.body.id;
+      const userRating = await bookRatingsData.hasUserRatedBook(userId, bookId);
+      res.json({ rating: userRating?.rating || 0 });
+    })
   )
 
   // Get reviews for book
-  .get("/:id/reviews", authMiddleware, loggedUserGuard, async (req, res) => {
-    try {
+  .get(
+    "/:id/reviews",
+    authMiddleware,
+    loggedUserGuard,
+    asyncHandler(async (req, res) => {
       const { id } = req.params;
       const reviews = await reviewsData.getReviewsForBook(id);
       if (!reviews) {
@@ -67,54 +66,50 @@ booksController
       } else {
         res.json(reviews);
       }
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  })
+    })
+  )
 
   // Create book review
   .post(
     "/:id/create-review",
     authMiddleware,
     loggedUserGuard,
-    async (req, res) => {
-      try {
-        const bookId = req.params.id;
-        const newReview = req.body.newReview;
-        const userId = req.body.user.id;
-        await reviewsData.createReview(bookId, newReview, userId);
-        res.end();
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
-    }
+    asyncHandler(async (req, res) => {
+      const bookId = req.params.id;
+      const newReview = req.body.newReview;
+      const userId = req.body.user.id;
+      await reviewsData.createReview(bookId, newReview, userId);
+      res.status(204).end();
+    })
   )
 
   // Rate book
-  .patch("/:id/rating", authMiddleware, loggedUserGuard, async (req, res) => {
-    try {
+  .patch(
+    "/:id/rating",
+    authMiddleware,
+    loggedUserGuard,
+    asyncHandler(async (req, res) => {
       const bookId = req.params.id;
       const { newRating, user } = req.body;
       await booksData.updateBookRating(bookId, newRating);
       await bookRatingsData.addBookRating(bookId, user.id, newRating);
       const newBookData = await booksData.getBookById(bookId);
       res.status(200).json(newBookData);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  })
+    })
+  )
 
   // Delete a book (as admin)
-  .delete("/:id", authMiddleware, loggedUserGuard, async (req, res) => {
-    try {
+  .delete(
+    "/:id",
+    authMiddleware,
+    loggedUserGuard,
+    asyncHandler(async (req, res) => {
       const bookId = req.params.id;
       const isDeleted = await booksData.deleteBook(bookId);
       if (isDeleted) {
         return res.status(204).end();
       }
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
+    })
+  );
 
 export default booksController;
