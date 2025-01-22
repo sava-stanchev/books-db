b5a9f22a65fcdf778e44cc001f04dab94850f700
Sava Stanchev <s.stanchev94@gmail.com>
2025-01-22 20:49:34 +0200

    convert expressjs index file to typescript

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,31 @@
-import express from "express";
+import express, { Express } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import passport from "passport";
-import jwtStrategy from "./auth/strategy.js";
+import jwtStrategy from "./auth/strategy";
 import booksController from "./controllers/books-controller.js";
 import authController from "./controllers/auth-controller.js";
 import usersController from "./controllers/users-controller.js";
 import reviewsController from "./controllers/reviews-controller.js";
 
 const config = dotenv.config().parsed;
-const PORT = config.PORT;
-const app = express();
+
+if (!config?.PORT) {
+  throw new Error("Environment variable PORT is missing.");
+}
+
+const PORT: number = parseInt(config.PORT, 10);
+
+const app: Express = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+
 passport.use(jwtStrategy);
 app.use(passport.initialize());
+
 app.use("/", authController);
 app.use("/books", booksController);
 app.use("/users", usersController);
